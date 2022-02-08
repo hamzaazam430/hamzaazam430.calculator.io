04d813ad56b2af4a37dd07e4d4323fd1cf59224d
Hamza Azam <hamzaazam430@outlook.com>
2022-02-08 19:20:16 +0500

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("amount");
+
+input.addEventListener("keypress", (e)=>{
+    if (e.key === "Enter") {
+        var input = parseInt(e.target.value);
+        var result = document.getElementById("result");
+    
+        input =  input + (input * 0.2);
+    
+        result.innerText = input;
+        e.target.value = "";
+    }
+});
+
+// function calculateAmount(){
+//     var input = parseInt(document.getElementById("amount").value);
+//     var result = document.getElementById("result");
+
+//     input =  input + (input * 0.2);
+
+//     result.innerText = input;
+// }
